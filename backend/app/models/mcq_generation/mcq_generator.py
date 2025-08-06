b4ea7506fe64ebeaed3478f49419e50bb0cb5eec
Aayush Kulkarni <aayushmkulkarni.5678@gmail.com>
2025-08-06 18:51:44 +0530

    added llama3.1 instead of openhermes

--- backend/app/models/mcq_generation/mcq_generator.py
+++ backend/app/models/mcq_generation/mcq_generator.py
@@ -5,7 +5,7 @@ import time
 
 TIMEOUT = 100
 MAX_TRIES = 3
-MODEL_NAME = "openhermes"
+MODEL_NAME = "llama3.1"
 OLLAMA_URL = "http://localhost:11434/api/generate"
 PATH_TO_TEXT = "app/models/mcq_generation/OCR_text.txt"
 
